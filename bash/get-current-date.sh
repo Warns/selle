a906b422fff3b9e3f9d5576f8573085fc07818d1
Mert <snickns@gmail.com>
2020-10-21 17:44:28 +0300

    current date bash

--- bash/get-current-date.sh
+++ bash/get-current-date.sh
@@ -3,3 +3,8 @@ Year=`date +%Y`
 Month=`date +%m`
 Day=`date +%d`
 Hour=`date +%H`
+Minute=`date +%M`
+Second=`date +%S`
+echo `date`
+echo "Current Date is: $Day-$Month-$Year"
+echo "Current Time is: $Hour:$Minute:$Second"
